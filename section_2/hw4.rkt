64ac4687efd23bb115c1850ffd9bcb0078a52304
xbfool <xbfool@gmail.com>
2020-09-03 23:31:27 +0800

    all done

--- section_2/hw4.rkt
+++ section_2/hw4.rkt
@@ -5,13 +5,11 @@
 
 ;; put your code below
 
-;; 1
 (define (sequence low high stride)
   (if (> low high)
       '()
       (cons low (sequence (+ low stride) high stride))))
 
-;; 2
 (define (string-append-map xs suffix)
   (map (lambda (x)
          (string-append x suffix))
@@ -25,7 +23,7 @@
 (define (stream-for-n-steps s n)
   (if (= 0 n)
       '()
-      (cons (car s) (stream-for-n-steps (cdr s) (- n 1)))))
+      (cons (car (s)) (stream-for-n-steps (cdr (s)) (- n 1)))))
 
 (define funny-number-stream
   (letrec ([f (lambda (x) (if (= 0 (remainder x 5))
@@ -34,17 +32,50 @@
     (lambda () (f 1))))
 
 (define dan-then-dog
-  (letrec ([f (lambda (x) (if (= x "dan.jpg")
-                              (cons x (lambda () (f ("dog.jpg"))))
-                              (cons x (lambda () (f ("dan.jpg"))))))])
+  (letrec ([f (lambda (x) (if (equal? x "dan.jpg")
+                              (cons x (lambda () (f "dog.jpg")))
+                              (cons x (lambda () (f "dan.jpg")))))])
     (lambda () (f "dan.jpg"))))
 
 (define (stream-add-zero s)
-  (letrec ([f (lambda (x) (cons (cons 0 (car x)) (lambda() (f (cdr x)))))])
+  (letrec ([f (lambda (x) (cons (cons 0 (car (x))) (lambda() (f (cdr (x))))))])
     (lambda () (f s))))
 
 (define (cycle-lists xs ys)
   (letrec ([f (lambda (x y n)
                 (cons (cons (list-nth-mod x n) (list-nth-mod y n))
-                      (lambda () (f (x y (+ 1 n))))))])
-    (lambda () (xs ys 0))))
+                      (lambda () (f x y (+ 1 n)))))])
+    (lambda () (f xs ys 0))))
+
+(define (vector-assoc v vec)
+  (letrec ([f (lambda (v vec n)
+              (cond [(>= n (vector-length vec)) #f]
+                    [else (let ([r (vector-ref vec n)])
+                       (if (pair? r)
+                           (if (equal? v (car r))
+                             r
+                             (f v vec (+ 1 n)))
+                           (f v vec (+ 1 n))))]))])
+    (f v vec 0)))
+
+(define (cached-assoc xs n)
+  (letrec ([vec (make-vector n #f)]
+           [i 0]
+           [f (lambda (v)
+                (let ([r (vector-assoc v vec)])
+                (if (equal? #f r)
+                    (let ([r1 (assoc v xs)])
+                      (if (equal? r1 #f)
+                          #f
+                          (let ([r2 (car r1)])
+                            (vector-set! vec i r1)
+                            (if (= i (- n 1))
+                                (set! i 0)
+                                (set! i (+ 1 i)))
+                            r1)))
+                    r)))])
+    f))
+               
+                         
+              
+                  
